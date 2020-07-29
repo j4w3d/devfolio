a4bb5ec005ec7b215ff081c286d21d1766b41cb0
Jawed Salim <12079219+j4w3d@users.noreply.github.com>
2020-07-29 18:09:46 +0530

    Fix validation check of subject

--- resume/validation.py
+++ resume/validation.py
@@ -4,6 +4,6 @@ from django import forms
 class FormValidation(forms.Form):
      name = forms.CharField(min_length=4, max_length=50)
      email = forms.EmailField(required=True)
-     subject = forms.CharField(min_length=8, max_length=127)
-     message = forms.CharField(min_length=4, max_length=255)
+     subject = forms.CharField(min_length=4, max_length=127)
+     message = forms.CharField(min_length=2, max_length=255)
 
