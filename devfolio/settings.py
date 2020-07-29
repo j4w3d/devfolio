3e550d1521138b2095627cf6b9afe21770998068
Jawed Salim <12079219+j4w3d@users.noreply.github.com>
2020-07-29 18:29:05 +0530

    Add: configure heroku postgres addon

--- devfolio/settings.py
+++ devfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from resume.settings import *
 
@@ -76,14 +77,7 @@ WSGI_APPLICATION = 'devfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
